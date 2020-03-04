4d816c2e763fafd80385d10f6083ff1c3761f7ca
Naufal Andika <andikaa.naufal@gmail.com>
2020-03-04 20:33:49 +0700

    Use user router

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('./db/mongoose')
 const companyRouter = require('./routes/company')
+const userRouter = require('./routes/user')
 
 const app = express()
 
 app.use(express.json())
 app.use(companyRouter)
+app.use(userRouter)
 
 module.exports = app
